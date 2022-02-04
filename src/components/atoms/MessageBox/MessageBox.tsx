f6973c19be2a77bd8ef9b261b0e923729aac5d4c
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2022-02-04 16:51:51 +0100

    Fixed small css issues with text overflowing from chat and post divs

--- src/components/atoms/MessageBox/MessageBox.tsx
+++ src/components/atoms/MessageBox/MessageBox.tsx
@@ -12,10 +12,10 @@ const currentUser = localStorage.getItem('username');
 
 export const MessageBox: FC<MessageBoxProps> = ({username, text, timestamp}) => {
   return (
-    <div className={`message-container ${username == currentUser && 'self'}`}>
+    <div className="message-container">
       <p className={`message-username ${username == currentUser && 'self'}`}>{username}</p>
-      <p className="message-text">{text}</p>
-      <p className="message-timestamp">{timestamp}</p>
+      <p className={`message-text ${username == currentUser && 'self'}`}>{text}</p>
+      <p className={`message-timestamp ${username == currentUser && 'self'}`}>{timestamp}</p>
     </div>
   );
 };
