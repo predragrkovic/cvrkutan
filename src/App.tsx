355e4af91334d944bb00741b78704744cd09421b
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2022-02-10 12:55:34 +0100

    Added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,22 @@
 // import PostCardContent from 'components/molecules/PostCardContent';
-import BottomTabNavigator from 'components/molecules/BottomTabNavigator';
-import ChatContainer from 'components/organisms/ChatContainer';
 import Header from 'components/organisms/Header';
-import PostsContainer from 'components/organisms/PostsContainer';
 import UserIdentificationModal from 'components/organisms/UserIdentification';
 import {useDarkTheme} from 'hooks/useDarkTheme';
-import {useState} from 'react';
+import Auth from 'pages/Auth';
+// import Home from 'pages/Home';
+// import {BrowserRouter as Router, Routes} from 'react-router-dom';
 import './App.scss';
 
 function App() {
   const {darkTheme} = useDarkTheme();
-  const [renderChoice, setRenderChoice] = useState<'chat' | 'posts'>('posts');
-
-  const handlePostsClick = () => {
-    setRenderChoice('posts');
-  };
-
-  const handleChatClick = () => {
-    setRenderChoice('chat');
-  };
 
   return (
     <div className={`app ${darkTheme && 'dark'}`}>
       <Header />
-      <div className={`screen-container`}>
-        <div className="padding-div"></div>
-        <PostsContainer isRendered={renderChoice === 'posts'} />
-        <ChatContainer isRendered={renderChoice === 'chat'} />
-        <div className="padding-div"></div>
+      <div className="screen-container">
+        {/* <Home /> */}
+        <Auth />
       </div>
-      <BottomTabNavigator
-        renderChoice={renderChoice}
-        handleChatClick={handleChatClick}
-        handlePostsClick={handlePostsClick}
-      />
       <UserIdentificationModal />
     </div>
   );
